135fa6180c6d0c4ca467d8889d79018895f47850
PlaidFunction1010 <kahootgogogo@gmail.com>
2025-11-03 14:38:20 +0800

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,8 +0,0 @@
-import { buildSidebar } from './sidebar.js';
-import { initKatexAutoRender } from './katex-init.js';
-export function initPage(){
-  buildSidebar('sidebar', location.pathname);
-  const fs = localStorage.getItem('fontSize') || 'large';
-  const content = document.getElementById('content'); content.classList.add(fs === 'small' ? 'fs-small' : 'fs-large');
-  initKatexAutoRender();
-}
--- assets/js/shell.js
+++
@@ -1,7 +0,0 @@
-export function mountShell(){
-  const header = document.getElementById('site-header');
-  if (header) header.innerHTML = `<div class="inner"><div class="brand"><div class="logo"></div><div>高中數學練習平台</div></div>
-  <div class="header-actions"><a class="btn" href="./index.html">首頁</a><a class="btn" href="./g11/unit1/tri-extrema3.html">高二·三角函數</a></div></div>`;
-  const footer = document.getElementById('site-footer');
-  if (footer) footer.innerHTML = `<div class="inner">© ${new Date().getFullYear()} 高中數學練習平台 · KaTeX · Noto Sans TC</div>`;
-}
