acc290f23c07b2eab249bcaa1edfdfca2f151178
PlaidFunction1010 <kahootgogogo@gmail.com>
2025-11-06 13:17:07 +0800

    Rename questions.js to questions_SAT114A.js

---
+++ questions_SAT114A.js
@@ -0,0 +1,7 @@
+window.__mountApp = function(){
+  const questions = [
+    {
+      id: "Q1",
+      type: "single",
+      score: 5,
+      text: "不透明袋中有藍、綠色球各若干顆，球上皆有1或2的編號，其顆數如下表：<br><table><tr><th></th><th>藍</th><th>綠</th></tr><tr><td>1號</td><td>2</td><td>4</td></tr><tr
